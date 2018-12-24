5d8111731797f62ede02b9446e919dfc7896dbe4
Doug Bacelar <dougbacelar@gmail.com>
2018-12-24 19:29:00 +0000

    feat: adding featured imaged on posts (#42)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ export default ({ children }) => (
   <main
     style={{
       margin: '0 auto',
-      maxWidth: '45rem',
+      maxWidth: '47rem',
       padding: '0 1rem 4rem 1rem',
     }}>
     {children}
