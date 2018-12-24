cc76d65b7397faaa11b9868ce461f790c505f056
Doug Bacelar <dougbacelar@gmail.com>
2018-12-24 13:10:13 +0000

    feat: add GitHub icon link (#40)
    * feat: adding edit page link
    
    * adding github icon link

--- src/components/EditPageLink.js
+++ src/components/EditPageLink.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import EditIcon from './icons/EditIcon';
+import GitHubIcon from './icons/GitHubIcon';
 
 const GITHUB_PAGES_LINK =
   'https://github.com/dougbacelar/methodcall/blob/master/src/pages';
 
 const getEditPageLink = (slug) => `${GITHUB_PAGES_LINK}${slug.slice(0, -1)}.md`;
 
-export default ({ slug }) => (
+export default ({ slug, style, useGithubIcon }) => (
   <a
     href={getEditPageLink(slug)}
     rel='noopener noreferrer'
-    style={{ backgroundImage: 'none', display: 'flex', float: 'right' }}
+    style={{
+      backgroundImage: 'none',
+      display: 'flex',
+      marginLeft: '0.3rem',
+      ...style,
+    }}
     target='_blank'>
-    <EditIcon />
-    edit this page on GitHub
+    {useGithubIcon ? (
+      <GitHubIcon />
+    ) : (
+      <>
+        <EditIcon />
+        edit this page on GitHub
+      </>
+    )}
   </a>
 );
