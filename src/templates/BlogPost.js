4b67e805a640fcb96c1bd015069c3c144be697fe
Doug <dougbacelar@gmail.com>
2018-12-24 19:05:41 +0000

    feat: adding featured imaged on posts

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import EditPageLink from '../components/EditPageLink';
 import converDateString from '../utils/date';
+import Img from 'gatsby-image';
 
 export default ({ data }) => {
   const post = data.markdownRemark;
@@ -16,6 +17,10 @@ export default ({ data }) => {
           <small>{converDateString(dateString)}</small>
         </time>
         <EditPageLink slug={post.fields.slug} useGithubIcon={true} />
+        <Img
+          sizes={post.frontmatter.featuredImage.childImageSharp.sizes}
+          style={{ maxHeight: '30rem' }}
+        />
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         <EditPageLink
           slug={post.fields.slug}
@@ -38,6 +43,13 @@ export const query = graphql`
       }
       frontmatter {
         date
+        featuredImage {
+          childImageSharp {
+            sizes(maxWidth: 900) {
+              ...GatsbyImageSharpSizes
+            }
+          }
+        }
         title
       }
     }
